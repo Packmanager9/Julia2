e8bddd85e0aad03e02d87b2678e9a6bf0b178a66
Packmanager9 <36295363+Packmanager9@users.noreply.github.com>
2021-03-09 07:02:35 -0800

    Add files via upload

--- julia2.js
+++ julia2.js
@@ -1,49 +1,6 @@
-
 window.addEventListener('DOMContentLoaded', (event) => {
-    let r = 5
-    let tutorial_canvas = document.getElementById("tutorial");
-    let tutorial_canvas_context = tutorial_canvas.getContext('2d');
-    tutorial_canvas.addEventListener('pointermove', continued_stimuli);
-    function continued_stimuli(e) {
-        FLEX_engine = tutorial_canvas.getBoundingClientRect();
-        XS_engine = e.clientX - FLEX_engine.left;
-        YS_engine = e.clientY - FLEX_engine.top;
-        TIP_engine.x = XS_engine
-        TIP_engine.y = YS_engine
-        TIP_engine.body = TIP_engine
-        rect.color = "black"
-        rect.draw()
-        const imageData = tutorial_canvas_context.getImageData(0, 0, tutorial_canvas.width, tutorial_canvas.height);
-        const data = imageData.data;
-        for (var i = 0; i < data.length; i += 12) {
-            let obj = indexer(i + (Math.random() * 0))
-            let comp = new Rectangle(obj.x, obj.y, 1, 1, 0)
-            comp = calcSpot(comp)
-            data[i] = comp.r // red
-            data[i + 1] = comp.g  // green
-            data[i + 2] = comp.b // blue
-            data[i + 3] = comp.a * 255
-        }
-        tutorial_canvas_context.putImageData(imageData, 0, 0);
-    }
-    let TIP_engine = {}
-    TIP_engine.x = tutorial_canvas.width * .5
-    TIP_engine.y = tutorial_canvas.height * .5
-    tutorial_canvas.style.background = "black"
-    class Rectangle {
-        constructor(x, y, height, width, step) {
-            this.x = x
-            this.y = y
-            this.height = height
-            this.width = width
-            this.complex = new Complex(((x - tutorial_canvas.width / 2) / tutorial_canvas.width) * 3.5, ((y - tutorial_canvas.height / 2) / tutorial_canvas.height) * 3.5)
-            this.color = 'rgb(0,0,0)'
-        }
-        draw() {
-            tutorial_canvas_context.fillStyle = this.color
-            tutorial_canvas_context.fillRect(this.x, this.y, this.width, this.height)
-        }
-    }
+    
+    let speed = -1
     class Complex {
         constructor(r = 0, i = 0) {
             this.imaginary = i
@@ -80,39 +37,157 @@ window.addEventListener('DOMContentLoaded', (event) => {
             return new Complex(real, imaginary)
         }
     }
-    let rect = new Rectangle(0, 0, tutorial_canvas.width, tutorial_canvas.height, 0)
-    let punch = new Complex(-.991, .2675)
+	
+	class Rectangle {
+        constructor(x, y, height, width) {
+            this.x = x
+            this.y = y
+            this.height = height
+            this.width = width
+            this.complex = new Complex(x * domain - zoom, y * range - zoom)
+            this.color = 'rgb(0,0,0)'
+        }
+        draw() {
+            tutorial_canvas_context.fillStyle = this.color
+            tutorial_canvas_context.fillRect(this.x, this.y, this.width, this.height)
+        }
+    }
+	
+    const r = 8
+	const r2 = r*r;
+    let tutorial_canvas = document.getElementById("tutorial");
+    let redval = document.getElementById("red");
+    let redx = 0
+    let greenval = document.getElementById("green");
+    let greenx = 0
+    let blueval = document.getElementById("blue");
+    let bluex = 0
+    let speedr = document.getElementById("speed");
+    speedr.onclick = flipper
+    let tutorial_canvas_context = tutorial_canvas.getContext('2d');
+    tutorial_canvas.addEventListener('pointermove', positionUpdate);
+	tutorial_canvas.addEventListener('wheel', zoomUpdate);
+    
+    function flipper(){
+        speed*=-1
+    }
+	let halfCanvasW = tutorial_canvas.width * 0.5
+	let halfCanvasH = tutorial_canvas.height * 0.5
+	let imgOffset
+	let realOffset
+	
+	let TIP_engine = {}
+    TIP_engine.x = halfCanvasW
+    TIP_engine.y = halfCanvasH
+    tutorial_canvas.style.background = "black"
+	
+	const imageData = tutorial_canvas_context.getImageData(0, 0, tutorial_canvas.width, tutorial_canvas.height);
+	const data = imageData.data;
+	
+	let zoom = 3.5/2
+	let domain = zoom / halfCanvasW
+	let range = zoom / halfCanvasH
+	
+	let rect = new Rectangle(0, 0, tutorial_canvas.width, tutorial_canvas.height, 0)
+	const punch = new Complex(-1.0, 0.0)
+	
+
+	
+	function positionUpdate(event) {
+        FLEX_engine = tutorial_canvas.getBoundingClientRect();
+        XS_engine = event.clientX - FLEX_engine.left;
+        YS_engine = event.clientY - FLEX_engine.top;
+        TIP_engine.x = XS_engine
+        TIP_engine.y = YS_engine
+        TIP_engine.body = TIP_engine
+        rect.color = "black"
+        rect.draw()
+		
+		halfCanvasW = tutorial_canvas.width * .5
+		halfCanvasH = tutorial_canvas.height * .5
+		
+		imgOffset =  punch.imaginary - XS_engine/ halfCanvasW + 1
+		realOffset = punch.real * punch.imaginary + YS_engine/ halfCanvasH - 1
+        redx = redval.value
+        greenx = greenval.value
+        bluex = blueval.value
+		continued_stimuli()
+	}
+	
+	function zoomUpdate(event) {
+		return //Doesn't really add anything, so skipped
+		
+		event.preventDefault();
+
+		zoom += event.deltaY * -0.001;
+		
+		zoom = Math.min(Math.max(0.01, zoom), 3.5);
+		  
+		continued_stimuli()
+	}
+	
+    function continued_stimuli() {
+
+		domain = zoom / halfCanvasW
+		range = zoom / halfCanvasH
+        
+		let X = 0
+		let Y = 0
+		const skip = 1
+        const skipP = skip * 4
+        if(speed == -1){
+            for (var i = 0; i < data.length; i += skipP) {
+                X += skip;
+                if(X >= tutorial_canvas.width){
+                    X = 0
+                    Y += 1
+                }
+                let comp = new Rectangle(X, Y, 1, 1)
+                comp = calcSpot(comp)
+                data[i] = comp.r // red
+                data[i + 1] = comp.g  // green
+                data[i + 2] = comp.b // blue
+                data[i + 3] = comp.a * 255
+            }
+        }else{
+        for (var i = 0; i < data.length; i += 8) {
+			X += 2;
+			if(X >= tutorial_canvas.width){
+				X = 0
+				Y += 1
+            }
+            let comp = new Rectangle(X, Y, 1, 1)
+            comp = calcSpot(comp)
+            data[i] = comp.r // red
+            data[i + 1] = comp.g  // green
+            data[i + 2] = comp.b // blue
+            data[i + 3] = comp.a * 255
+        }
+        }
+
+        tutorial_canvas_context.clearRect(0,0,tutorial_canvas.width, tutorial_canvas.height)
+        tutorial_canvas_context.putImageData(imageData, 0, 0);
+    }
+	
     function calcSpot(spot) {
         let iet = 0
-        let a = 0
-        while (iet < 32 && ((spot.complex.real * spot.complex.real) - (spot.complex.imaginary * spot.complex.imaginary)) < r * r) {
+		let xtemp = (spot.complex.real * spot.complex.real) - (spot.complex.imaginary * spot.complex.imaginary);
+		
+        while (iet < 64 && xtemp < r2) {
             iet++
-            let xtemp = (spot.complex.real * spot.complex.real) - (spot.complex.imaginary * spot.complex.imaginary)
-            spot.complex.imaginary = (2 * spot.complex.real * spot.complex.imaginary) + punch.imaginary
-            spot.complex.real = (xtemp) + (punch.real * punch.imaginary)
-
-            spot.complex.imaginary -= (TIP_engine.x - (tutorial_canvas.width * .5)) / (tutorial_canvas.width * .5)
-            spot.complex.real += ((TIP_engine.y - (tutorial_canvas.height * .5)) / (tutorial_canvas.height * .5))
-            a += .06
-            if (a > .48) {
-                a = 1
-            }
+            spot.complex.imaginary = (2 * spot.complex.real * spot.complex.imaginary) + imgOffset
+            spot.complex.real = xtemp + realOffset
+            xtemp = (spot.complex.real * spot.complex.real) - (spot.complex.imaginary * spot.complex.imaginary)
+            // xtemp+=2
         }
-        if (a < .1) {
-            a = 0
-        }
-        spot.g = Math.max(128 - (iet * 2), 0) + 1
-        spot.r = Math.max(0 + (iet * 8), 0)
-        spot.b = Math.max(0 + (iet * 8), 0) 
-        spot.a = a
+        spot.g = Math.max(255 - (iet * greenx), 0) + 1
+        spot.r = Math.max((Math.sqrt((iet * redx))*20), 0)
+        spot.b = Math.max((Math.sqrt((iet * bluex))*10), 0) 
+        spot.a = Math.min((iet-1)/8, 1)
         return spot
     }
-    function indexer(num) {
-        let obj = {}
-        let x = Math.floor((num / 4) % tutorial_canvas.width)
-        let y = Math.floor((num / 4) / tutorial_canvas.height)
-        obj.x = x + Math.random()
-        obj.y = y
-        return obj
-    }
+
+    // window.setInterval(function () {
+    //     continued_stimuli
+    // }, 17)
 })
